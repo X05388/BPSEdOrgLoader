3f6625defaf9544897068fd651fc79ea572ae60c
X05388 <38865144+X05388@users.noreply.github.com>
2020-04-21 12:43:04 -0400

    changes to check field

--- BPS.EdOrg.Loader/BPS.EdOrg.Loader/Controller/StaffAssociationController.cs
+++ BPS.EdOrg.Loader/BPS.EdOrg.Loader/Controller/StaffAssociationController.cs
@@ -521,7 +521,7 @@ namespace BPS.EdOrg.Loader.Controller
 
                 if (_restServiceManager.IsSuccessStatusCode((int)response.StatusCode))
                 {
-                    if(staffData!= null && (!staffData.Any()))
+                    if(staffData!= null)
                     {
                         StaffReference rootObject = new StaffReference
                         {
